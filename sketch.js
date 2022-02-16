e11dee3051369685b58939c02e58e0098c478af2
GustavoKlas <99832259+GustavoKlas@users.noreply.github.com>
2022-02-16 15:44:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(1200,800);
+  quadrado= createSprite (600, 400, 50, 50)
+}
+
+function draw() 
+{
+  
+  if (keyIsDown ("D")){
+    console.log("red")
+  }
+
+drawSprites()
+}
+
+
+
+
